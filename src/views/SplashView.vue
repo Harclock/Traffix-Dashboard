aaa229c93bd9d56e09d611df7d2ef4839282a741
Harclock <lorenzo.senesi.it@gmail.com>
2026-02-23 11:15:14 +0100

    sto installando vite, attendete

--- src/views/SplashView.vue
+++ src/views/SplashView.vue
@@ -1,6 +1,16 @@
 <script setup>
 import '@/assets/styles/CSS/base.css'
 import '@/assets/styles/CSS/splash-screen.css'
+
+import { onMounted } from 'vue'
+
+const load = () => {
+  setTimeout(() => {
+    this.$router.push('/MainPage');
+  }, 3000); // Mostra lo splash per 3 secondi
+}
+
+onMounted(load)
 </script>
 
 <template>
